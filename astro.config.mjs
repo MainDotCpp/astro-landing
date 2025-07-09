489ca13f72d014c1ce4567bcb395b8be938c87e7
yangyang <1400002030@qq.com>
2025-07-09 15:42:24 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,19 @@
-import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
-
-import tailwindcss from '@tailwindcss/vite'
-
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import UnoCSS from 'unocss/astro'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
+  integrations: [react(), UnoCSS({
+    injectReset: true,
+  })],
   compressHTML: false,
   vite: {
-    // plugins: [tailwindcss()],
   },
   build: {
     assetsPrefix: 'https://assetfun.top',
     assets: 'static',
   },
-  // experimental: {
-  //   csp: {
-  //     scriptDirective: {
-  //       strictDynamic: true,
-  //     },
-  //   },
-  // },
 })
--- public/static/XoONOW.js
+++ public/static/XoONOW.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const linkBtn = document.querySelectorAll('.link-btn')
   linkBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      if (typeof gtag_report_conversion === 'function') { gtag_report_conversion(link) }
-      else { window.location.href = link }
-    })
+    btn.addEventListener('click', onLinkBtnClick)
   })
 })
+
+function onLinkBtnClick() {
+  if (typeof gtag_report_conversion === 'function') { gtag_report_conversion(link) }
+  else { window.location.href = link }
+}
