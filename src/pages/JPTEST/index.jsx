82e5c451aa377de1c692ab3f163c4eebb3610e06
yangyang <1400002030@qq.com>
2025-07-11 23:25:04 +0800

    init

--- src/pages/JPTEST/index.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react'
-
-export default function JPTEST() {
-  const [name, setName] = useState('JPTEST')
-  useEffect(() => {
-    setName('JPTEST2')
-  }, [])
-  return <div>{name}</div>
-}
