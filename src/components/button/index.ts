4b562cd5658e5e39fcb4cb506a4d875c0f9ff5d7
杨洋 <yy@yangyangdeMacBook-Air.local>
2025-12-09 19:25:14 +0800

    1

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,6 +1,14 @@
 // 按钮组件导出
 export { default as BandButton } from './BandButton.astro'
+export { default as BandCtaButton } from './BandCtaButton.astro'
+// CTA 按钮组件导出
+export { default as BaseCtaButton } from './BaseCtaButton.astro'
 export { default as KakaoCopyButton } from './KaKaoCopyButton.tsx'
+
+export { default as KakaoCtaButton } from './KakaoCtaButton.astro'
+export { default as MixedCtaButton } from './MixedCtaButton.astro'
+export { default as ModalBandButton } from './ModalBandButton.astro'
+export { default as ModalKakaoButton } from './ModalKakaoButton.astro'
 
 // 类型导出
 export interface BandButtonProps {
